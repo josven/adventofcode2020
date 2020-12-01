ff35455173ee5376c65021410de946be2be63a1b
Jonas Svensson <jonas.svensson@disneystreaming.com>
2020-12-01 21:02:02 +0100

    day one more compact

--- day1/a.py
+++ day1/a.py
@@ -4,7 +4,5 @@ import itertools
 with open("input.txt",'r') as file:
     all_lines = file.readlines()
     combinations = itertools.combinations(map(int, all_lines), 2)
-    for a, b in combinations:
-        if a + b == 2020:
-            print(a * b)
-
+    a, b = next(iter(filter(lambda (a, b) : a + b == 2020, combinations)))
+    print(a * b)
--- day1/b.py
+++ day1/b.py
@@ -4,7 +4,5 @@ import itertools
 with open("input.txt",'r') as file:
     all_lines = file.readlines()
     combinations = itertools.combinations(map(int, all_lines), 3)
-    for a, b, c in combinations:
-        if a + b + c == 2020:
-            print(a * b * c)
-
+    a, b, c = next(iter(filter(lambda (a, b, c) : a + b + c == 2020, combinations)))
+    print(a * b * c)
