67283fdc3a94c827762c68b347066d1186385f54
Jonas Svensson <jonas.svensson@disneystreaming.com>
2020-12-01 21:08:33 +0100

    Add time

--- day1/a.py
+++ day1/a.py
@@ -1,8 +1,12 @@
 import itertools
+import time
 
 
+t0 = time.time()
 with open("input.txt",'r') as file:
     all_lines = file.readlines()
     combinations = itertools.combinations(map(int, all_lines), 2)
     a, b = next(iter(filter(lambda (a, b) : a + b == 2020, combinations)))
     print(a * b)
+t1 = time.time()
+print(t1 - t0)
--- day1/b.py
+++ day1/b.py
@@ -1,8 +1,12 @@
 import itertools
+import time
 
 
+t0 = time.time()
 with open("input.txt",'r') as file:
     all_lines = file.readlines()
     combinations = itertools.combinations(map(int, all_lines), 3)
     a, b, c = next(iter(filter(lambda (a, b, c) : a + b + c == 2020, combinations)))
     print(a * b * c)
+t1 = time.time()
+print(t1 - t0)
